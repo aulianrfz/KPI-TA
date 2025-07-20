bf8c681680feb1a596ba523d0e66e07525d763d0
fathiaqurrata <123913816+fathiaqurrata@users.noreply.github.com>
2025-07-20 09:20:42 +0700

    .

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,34 +1,37 @@
 <footer class="mt-5 py-5" style="background: linear-gradient(90deg, #0367A6, #3F9BBF);">
     <div class="container">
-        <div class="row">
-            <div class="col-md-4 mb-3">
-                <div class="fw-bold">Nama.com</div>
+        <div class="row text-white">
+            <div class="col-12 col-md-4 mb-4">
+                <div class="fw-bold fs-5 fs-md-3">Nama.com</div>
             </div>
-            <div class="col-md-4 mb-3">
+
+            <div class="col-6 col-md-4 mb-4">
                 <div class="footer-links">
-                    <p><a href="#" class="text-white text-decoration-none">About</a></p>
-                    <p><a href="#" class="text-white text-decoration-none">How it works</a></p>
-                    <p><a href="#" class="text-white text-decoration-none">Careers</a></p>
-                    <p><a href="#" class="text-white text-decoration-none">Press</a></p>
-                    <p><a href="#" class="text-white text-decoration-none">Blog</a></p>
-                    <p><a href="#" class="text-white text-decoration-none">Forum</a></p>
+                    <p class="mb-1"><a href="#" class="text-white text-decoration-none small">About</a></p>
+                    <p class="mb-1"><a href="#" class="text-white text-decoration-none small">How it works</a></p>
+                    <p class="mb-1"><a href="#" class="text-white text-decoration-none small">Careers</a></p>
+                    <p class="mb-1"><a href="#" class="text-white text-decoration-none small">Press</a></p>
+                    <p class="mb-1"><a href="#" class="text-white text-decoration-none small">Blog</a></p>
+                    <p class="mb-1"><a href="#" class="text-white text-decoration-none small">Forum</a></p>
                 </div>
             </div>
-            <div class="col-md-4 mb-3">
+
+            <div class="col-6 col-md-4 mb-4">
                 <div class="footer-links">
-                    <p><a href="#" class="text-white text-decoration-none">Partner with us</a></p>
-                    <p><a href="#" class="text-white text-decoration-none">Affiliate program</a></p>
-                    <p><a href="#" class="text-white text-decoration-none">Community partners</a></p>
-                    <p><a href="#" class="text-white text-decoration-none">Promotions</a></p>
-                    <p><a href="#" class="text-white text-decoration-none">Integrations</a></p>
-                    <p><a href="#" class="text-white text-decoration-none">Loyalty program</a></p>
+                    <p class="mb-1"><a href="#" class="text-white text-decoration-none small">Partner with us</a></p>
+                    <p class="mb-1"><a href="#" class="text-white text-decoration-none small">Affiliate program</a></p>
+                    <p class="mb-1"><a href="#" class="text-white text-decoration-none small">Community partners</a></p>
+                    <p class="mb-1"><a href="#" class="text-white text-decoration-none small">Promotions</a></p>
+                    <p class="mb-1"><a href="#" class="text-white text-decoration-none small">Integrations</a></p>
+                    <p class="mb-1"><a href="#" class="text-white text-decoration-none small">Loyalty program</a></p>
                 </div>
             </div>
         </div>
-        <div class="mt-4 d-flex gap-3 justify-content-center">
-            <a href="#"><img src="https://img.icons8.com/ios-glyphs/30/ffffff/twitter--v1.png"/></a>
-            <a href="#"><img src="https://img.icons8.com/ios-glyphs/30/ffffff/instagram-new.png"/></a>
-            <a href="#"><img src="https://img.icons8.com/ios-glyphs/30/ffffff/facebook-new.png"/></a>
+
+        <div class="mt-3 d-flex justify-content-center gap-3">
+            <a href="#"><img src="https://img.icons8.com/ios-glyphs/24/ffffff/twitter--v1.png" alt="Twitter" class="img-fluid" style="max-width: 24px;"></a>
+            <a href="#"><img src="https://img.icons8.com/ios-glyphs/24/ffffff/instagram-new.png" alt="Instagram" class="img-fluid" style="max-width: 24px;"></a>
+            <a href="#"><img src="https://img.icons8.com/ios-glyphs/24/ffffff/facebook-new.png" alt="Facebook" class="img-fluid" style="max-width: 24px;"></a>
         </div>
     </div>
 </footer>
